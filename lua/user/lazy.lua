090590d76195cae6335b2e0d8de162c27de2a614
Jccc-l <1216550215Jc@gmail.com>
2023-05-25 14:02:00 +0800

    Add plugin treesitter

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -70,12 +70,19 @@ require("lazy").setup({
 	-- use({ "folke/tokyonight.nvim", commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764" })
 	"lunarvim/darkplus.nvim", --, commit = "13ef9daad28d3cf6c5e793acfc16ddbf456e1c83" })
 
+	------------------------
+	------------------------
+	--treesitter highlight--
+	------------------------
+	------------------------
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate"
+	},
 
 
 
-
-
-	"mhinz/vim-startify",  -- Start Screen
+	"mhinz/vim-startify", -- Start Screen
 
 
 
