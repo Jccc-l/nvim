98fdc446f34f4eda9d942a9ec38b3e6b5f144737
Jccc-l <1216550215Jc@gmail.com>
2023-05-27 12:59:59 +0800

    Create a new wiki

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -246,21 +246,6 @@ require("lazy").setup({
 		branch = "dev",
 		lazy = false,
 		-- dependencies = {'michal-h21/vim-zettel'},
-		init = function()
-			vim.g.vimwiki_global_ext = 0
-			vim.g.vimwiki_list = {
-				{
-					path = "~/vimwiki/",
-					syntax = "markdown",
-					ext = ".md",
-				},
-				{
-					path = "~/Documents/Note/",
-					syntax = "markdown",
-					ext = ".md",
-				},
-			}
-		end
 	},
 
 	-------
---
+++ lua/user/plugconf/vimwiki.lua
@@ -0,0 +1,18 @@
+vim.g.vimwiki_global_ext = 0
+vim.g.vimwiki_list = {
+	{
+		path = "~/vimwiki/",
+		syntax = "markdown",
+		ext = ".md",
+	},
+	{
+		path = "~/Documents/Note/",
+		syntax = "markdown",
+		ext = ".md",
+	},
+	{
+		path = "~/Documents/Note/English/",
+		syntax = "markdown",
+		ext = ".md",
+	},
+}
