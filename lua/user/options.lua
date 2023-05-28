24843d368a828e1878c79171c0e4128c7458a52e
Jccc-l <1216550215Jc@gmail.com>
2023-05-28 21:46:34 +0800

    Reset clipboard to default

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -12,7 +12,7 @@ M.load_default_options = function()
 
 	local default_options = {
 		backup = false,      -- creates a backup file
-		clipboard = "unnamedplus", -- allows neovim to access the system clipboard
+		-- clipboard = "unnamedplus", -- allows neovim to access the system clipboard
 		cmdheight = 1,       -- more space in the neovim command line for displaying messages
 		completeopt = { "menuone", "noselect" },
 		conceallevel = 1,    -- so that `` is visible in markdown files
