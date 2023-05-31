072f599153cc55df8ef7e2b2296d180ee6ddb680
Jccc-l <1216550215Jc@gmail.com>
2023-05-31 19:08:33 +0800

    Disable notify for markdown file

--- lua/user/keymap/md-snippets.lua
+++ lua/user/keymap/md-snippets.lua
@@ -1,7 +1,6 @@
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "markdown" },
 	callback = function()
-		vim.notify("This is a markdown file")
 		-- Bold
 		keymap("i", ",b", "****<++><ESC>F*hi", opts)
 		-- Italy
