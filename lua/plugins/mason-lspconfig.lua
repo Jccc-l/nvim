1303c36768bba588fe18c9edb15ef7ec6eca7666
Jccc-l <1216550215Jc@gmail.com>
2026-06-19 22:44:30 +0000

    Neovim: Automatically install LSP server.

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -3,4 +3,13 @@ require('mason-lspconfig').setup({
   automatic_enable = {
     'lua_ls',
   },
+  ensure_installed={
+    "pyright",
+    "lua_ls",
+    "stylua",
+    "jdtls",
+    "vimls",
+    "bashls",
+    "clangd",
+  }
 })
